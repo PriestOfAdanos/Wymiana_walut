9a18bce214ee885bc835ad50c5549cda318cab74
Pawel <31793531+PriestOfAdanos@users.noreply.github.com>
2019-10-11 08:57:06 +0200

    Add files via upload

--- waluty.py
+++ waluty.py
@@ -1,6 +1,3 @@
-import csv
-
-
 """
 A demonstration how to complicate a trivial task for no reason at all
 
@@ -8,54 +5,55 @@ A simple calculator that allows user to simulate
 currency exchange 
 
 INPUT:
-csv file 'currency_value.txt' in format currency_name, exchange_rate
-(what is single unit of said currency worth in relation to for example  USD)
+csv file 'currency_value.txt' in format <currency_name>,<exchange_rate>,
+Users Data, not case sensitive,
+(exchange_rate is a single unit of said currency worth in relation to for example  USD)
 
 OUTPUT:
 The amount of money you would receive if you tried to exchange some amount 
 of one currency to the other
+'Po zamianie <start_currency_name> <start_currency_amount> --> <end_currency_name> otrzymasz <end_currency_amount> <end_currency_name>'
 """
 
+
 with open('currency_value.txt') as currency_data:
-
-    # ideally done with dict comprehension(list comprehension for dicts, idk how its called :p
-    my_dict = dict()
-    for row in currency_data:
-        row = row.split(',')
-        my_dict[row[0]] = int(row[1])
+    # TBD redundant multiple split operations
+    currency_value_dict = {row.split(',')[0]: int(row.split(',')[1])for row in currency_data}
 
 
-def money_changer(currency_start, currency_start_amount, currency_end):
-    value = currency_start*currency_start_amount/currency_end
-    value = str(value)
-    currency_start_amount = str(currency_start_amount)
-
-    print(('Po zamianie {} {} --> {} otrzymasz {} {}').format(currency_start_amount,
-                                                               currency_start, currency_end, value, currency_end))
+def money_changer(args):
+    value = currency_value_dict[args[0]]*args[1]/currency_value_dict[args[2]]
+    print('Po zamianie {} {} --> {} otrzymasz {} {}'.format(args[1], args[0], args[2], value, args[2]))
 
 
 def data_cleaner(expected_type, text):
-    # obviously ugly, TBD
-    #
-    data = input(text)
+    # A simple input data validator
+    data = input(text).upper()
     if data == '':
+        print('Podaj odpowiedź')
         return data_cleaner(expected_type, text)
-    elif type(data) == expected_type:
-        return data
+    if data not in currency_value_dict and expected_type == str:
+        print('Niewłaściwe dane poczatkowe')
+        return data_cleaner(expected_type, text)
     try:
         return expected_type(data)
     except ValueError:
-        print('Coś poszło nie tak')
+        print('Pamiętaj oby dane były w formacie {}'.format(expected_type))
         return data_cleaner(expected_type, text)
 
 
 def get_data():
-    currency_start = data_cleaner(str, 'Podaj walutę z której chcesz przewalutować ')
-    currency_start_amount = data_cleaner(int, 'Ile jej masz? ')
-    currency_end = data_cleaner(str, 'Podaj walutę do której chcesz przewalutować ')
-    currency_start = my_dict[currency_start]
-    currency_end = my_dict[currency_end]
-    return currency_start, currency_start_amount, currency_end
+    cleaned_data = list()
+
+    texts = ['Podaj walutę z której chcesz przewalutować ',
+            'Ile jej masz? ',
+            'Podaj walutę do której chcesz przewalutować ']
+
+    types = [str, int, str]
+
+    for expected_type, message in zip(types, texts):
+        cleaned_data.append(data_cleaner(expected_type, message))
+    return cleaned_data
 
 
 def continue_loop():
@@ -63,34 +61,26 @@ def continue_loop():
     Asks user if they want to keep going or not until they give a proper answer,
     if overused ram iz gonna melt
     """
-    inputz = input('Jeszcze raz?(Y/N) ')
+    inputz = input('Jeszcze raz?(Y/N) ').upper()
     if inputz == 'Y':
         return True
     elif inputz == 'N':
-        return False
-    else:
-        return continue_loop()
+        return None
+    return continue_loop()
 
 
 def initial_info():
     """
-    prints out 10 keys in a row, than goes down a row,
-    should be done in 2 lines at worst
+    TBD prints out 10 keys in a row, than goes down a row,
     """
-    keys = [key for key in my_dict.keys()]
-    for i in range(len(keys)):
-        if i%9 == 0:
-            print('\n')
-        print(keys[i], end=',')
-    print('\n')
+    currency_list = list(currency_value_dict.keys())
+    print(currency_list)
 
 
 def main():
     initial_info()
     while continue_loop:
-        currency_start, currency_start_amount, currency_end = get_data()
-        money_changer(currency_start, currency_start_amount, currency_end)
-        continue_loop()
+        money_changer(get_data())
 
 
 main()
